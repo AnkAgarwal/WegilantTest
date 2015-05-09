f883ebbed11ed7057e12f496e82ae2e9943c4629
Ankit Agarwal <ankit.agarwal@ngiventures.com>
2015-05-09 20:02:54 +0530

    Added comments

--- Wegilant/src/com/wegilant/Main/Main.java
+++ Wegilant/src/com/wegilant/Main/Main.java
@@ -19,18 +19,27 @@ public class Main {
 
 	public static void main(String[] args){
 		
+		String filePath = "Sample.xml";
+		String mergeFilePath = "Master.xml";
+		String remoteFileURI = "https://file.ac/x_C6gIsigeU/Sample.xml?download=true";
+		
+		//ReadXMLFromFile is to read the file from particular path directory
 		ReadXMLFromFile parseFromFile = new ReadXMLFromFile();
-		List<Company> companyList = parseFromFile.readFile("Sample.xml");
+		List<Company> companyList = parseFromFile.readFile(filePath);
 		
+		//ReadXMLFromRemoteLocation is to read the file from particular remote server
 		ReadXMLFromRemoteLocation parseFromRemote = new ReadXMLFromRemoteLocation();
-		List<Employee> employeeList = parseFromRemote.readRemoteServerFile("https://file.ac/x_C6gIsigeU/Sample.xml?download=true");
+		List<Employee> employeeList = parseFromRemote.readRemoteServerFile(remoteFileURI);
 		
+		//MergeXML will merge the files based on the collection retrived by parsing the uper files
 		MergeXML mergeObject = new MergeXML();
-		mergeObject.mergeXML(companyList, employeeList, "Master.xml");
+		mergeObject.mergeXML(companyList, employeeList, mergeFilePath);
 		
+		//Merge file is parsed to obtain the collection
 		ReadXMLFromMasterFile parseFromMasterFile = new ReadXMLFromMasterFile();
-		List<Organisation> orgList = parseFromMasterFile.readFile("Master.xml");
+		List<Organisation> orgList = parseFromMasterFile.readFile(mergeFilePath);
 		
+		//blocking queue is created and producer consumer thread are initialsed
 		BlockingQueue<Organisation> queue = new ArrayBlockingQueue<>(3);
         Producer producer = new Producer(queue,orgList);
         Consumer consumer = new Consumer(queue);
